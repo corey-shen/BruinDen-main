636b3774b980f55f4f370468232e52dc47ae1971
Kelsey Shan <kelseyshan101@gmail.com>
2024-11-09 00:52:11 -0800

    Feat: Navigation bar skeleton
    Adding a basic text bar that appears at the top of the screen

---
+++ bruinden/app/components/navigation_bar/Navigationbar.tsx
@@ -0,0 +1,9 @@
+const Navigationbar = () =>{
+    return(
+        <div className = "fixed w-full bg-white z-10 shadow-sm">
+            I am a Navigation Bar
+        </div>
+    );
+}
+
+export default Navigationbar;
