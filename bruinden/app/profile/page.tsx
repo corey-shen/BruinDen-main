fde7a5835434e32e016bacd930630ce9244ab559
Kelsey Shan <kelseyshan101@gmail.com>
2024-12-05 23:09:28 -0800

    Remove part of profile changing

--- bruinden/app/profile/page.tsx
+++ bruinden/app/profile/page.tsx
@@ -91,19 +91,6 @@ const ProfilePage = () => {
     []
   );
 
-  const handleUniversityIdChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      const value = event.target.value;
-      if (/^\d*$/.test(value)) {
-        setUniversityId(value);
-        setIdError("");
-      } else {
-        setIdError("University ID must be a number");
-      }
-    },
-    []
-  );
-
   const userId = currentUser?.id;
   const name = firstName + " " + lastName;
 
@@ -200,18 +187,6 @@ const ProfilePage = () => {
               />
             </div>
           </div>
-
-          <div>
-            <label className="block text-gray-700 mb-2">Password</label>
-            <input
-              type="text"
-              value={universityId}
-              onChange={handleUniversityIdChange}
-              className="w-full py-2 px-4 border border-gray-300 rounded-md"
-            />
-            {idError && <p className="text-red-500 text-sm mt-1">{idError}</p>}
-          </div>
-
           <div>
             <label className="block text-gray-700 mb-2">College Year</label>
             <select
